91dcfa087caf436315d5cd94515b8094c83716c1
zak <sajeeve@gmail.com>
2016-05-17 20:23:30 -0500

    Added _C class

---
+++ src/org/java8/playground/_C.java
@@ -0,0 +1,15 @@
+package org.java8.playground;
+
+/**
+ * Created by zak on 5/17/16.
+ */
+public class _C {
+
+    private static int $;
+    public static void main(String[] main)
+    { String a_b = null; //initialize string
+        System.out.print($);
+        System.out.print(a_b);
+    }
+
+}
