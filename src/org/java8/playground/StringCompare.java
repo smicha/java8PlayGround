5424473886a2c9254f279e7303d3964bcbf26201
zak <sajeeve@gmail.com>
2016-05-17 20:29:47 -0500

    Added String Compare class

---
+++ src/org/java8/playground/StringCompare.java
@@ -0,0 +1,20 @@
+package org.java8.playground;
+
+/**
+ * Created by zak on 5/17/16.
+ */
+public class StringCompare {
+
+    public static void main(String[] args)
+    {
+
+        String s1 = "Java";
+        String s2 = "Java";
+        StringBuilder sb1 = new StringBuilder();
+        sb1.append("Ja").append("va");
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
+        System.out.println(sb1.toString() == s1);
+        System.out.println(sb1.toString().equals(s1));
+    }
+}
